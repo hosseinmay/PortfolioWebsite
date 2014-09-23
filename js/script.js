bcd2629e26efa623c7a6f5742947da68a9243fee
Hossein Mayanloo <mhossein2005@gmail.com>
2014-09-23 03:39:14 -0400

    Easter eggs

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log("Let me know if you see anything that could be improved in the website.  Thanks! - hossein@hosseinmayanloo.com");
+
+var funfacts = new Array();
+funfacts[0] = "The woolly mammoth was around when the pyramids were being built.";
+funfacts[1] = "'Facetiously' contains all 5 vowels and 'y' in alphabetical order.";
+funfacts[2] = "A group of ferrets is called a business.";
+funfacts[3] = "The continent with the highest average education level is Antarctica.";
+funfacts[4] = "In the middle ages, all children were called girls.";
+
+var random = Math.floor(Math.random() * 5);
+$(document).ready(function(){
+	$("#secret-text").text(funfacts[random]);
+	$("#fun-fact").show();
+
+	$(window).scroll(function() {
+		if($(window).scrollTop() + $(window).height() > $(document).height() - 35) {
+	   		$(window).unbind('scroll');
+	    	$("#secret-text").delay(2000).fadeIn(1000);
+	    	$("#and-javascript").delay(6000).fadeIn(1000);
+		}
+	});
+});
+
